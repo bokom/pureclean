5336aa094654949ce33cc8784d264dcf30bbd6c4
Petter Charles Redfern <pettercr@stud.ntnu.no>
2019-05-06 14:38:38 +0200

    Created react app and installed some prerequisites
    - Created react client
    - Installed Reactstrap
    - Installed UUID
    - Installed react transition group

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavbar></AppNavbar>
+      </div>
+    );
+  }
+}
+
+export default App;
